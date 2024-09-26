30f5ad0781af19d5f44809498f006dff4fac892e
Md Mahtab Ali <161712272+hrmahtab01@users.noreply.github.com>
2024-09-27 02:51:08 +0600

    group create done

--- src/Components/GroupListCom.jsx
+++ src/Components/GroupListCom.jsx
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CiSearch } from "react-icons/ci";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import progileimage from "../assets/mahtab.jpg";
-import Profileimage2 from "../assets/Signin.png"
-import profileimage3 from "../assets/Signup.jpg"
+import Profileimage2 from "../assets/Signin.png";
+import profileimage3 from "../assets/Signup.jpg";
 import { FaPlus } from "react-icons/fa6";
+import { getDatabase, push, ref, set ,onValue  } from "firebase/database";
+import { useSelector } from "react-redux";
+import { PacmanLoader } from "react-spinners";
+import { InfinitySpin,  } from "react-loader-spinner";
+
 
 const GroupListCom = () => {
+  const data = useSelector((state) => state.UserData.value);
+  const db = getDatabase();
+  let [CreateModal, SetCreateModal] = useState(false);
+  const [Groupname, Setgrupname] = useState("");
+  const [Loader, setloader] = useState(false);
+  const [Groupdata ,Setgroupdata]=useState([])
+
+  const HandleCreatModal = () => {
+    SetCreateModal(!CreateModal);
+  };
+
+  let HandleGroupname = (e) => {
+    Setgrupname(e.target.value);
+  };
+
+  const HandleSubmitGroup = () => {
+    set(push(ref(db, "grouplist/")), {
+      groupName: Groupname,
+      AdminName: data.displayName,
+    })
+      .then(() => {
+        setloader(true);
+      })
+      .then(() => {
+        setTimeout(() => {
+          setloader(false);
+          SetCreateModal(false);
+        }, 2000);
+      });
+  };
+
+  useEffect(()=>{
+    const groupref = ref(db, 'grouplist/');
+    onValue(groupref, (snapshot) => {
+     const grouparray =[]
+     snapshot.forEach((item)=>{
+      grouparray.push(item.val())
+     })
+     Setgroupdata(grouparray)
+    });
+  },[])
+
   return (
     <section>
       <div className="  ">
@@ -17,116 +64,76 @@ const GroupListCom = () => {
             className=" w-full h-full shadow-lg rounded-[20px] placeholder:absolute placeholder:top-2/4 placeholder:left-14 placeholder:translate-y-[-50%] outline-none type pl-16"
           />
           <CiSearch className="absolute top-2/4 left-4 translate-y-[-50%] text-[19px] text-ThirdColor font-bold" />
-          <BsThreeDotsVertical className="absolute right-0 top-2/4  translate-y-[-50%] text-Secondary text-lg" />
+          <BsThreeDotsVertical className="absolute right-0 top-2/4  translate-y-[-50%] text-Secondary text-lg " />
         </div>
 
-         <div className="max-w-[427px] shadow-xl rounded-[20px]  py-4 px-6  mt-[43px]">
+        <div className="max-w-[427px] shadow-xl rounded-[20px]  py-4 px-6  mt-[43px]">
           <div className="flex justify-between items-center">
             <h3 className="text-lg text-ThirdColor font-semibold font-Nunito">
-            Groups List
+              Groups List
             </h3>
-            <BsThreeDotsVertical className="text-Secondary" />
+            <BsThreeDotsVertical
+              onClick={HandleCreatModal}
+              className="text-Secondary"
+            />
           </div>
 
           <div className="w-full h-[300px] overflow-y-scroll cursor-pointer ">
-            <div className="flex justify-between items-center  border-b border-black/25 pb-3 mt-4">
-              <div className="flex gap-3 mt-[17px]">
-                <img
-                  src={Profileimage2}
-                  alt="progileimage"
-                  className="w-[52px] h-[52px] rounded-full object-cover"
+            {CreateModal ? (
+              <div className="flex flex-col mt-4">
+                <input
+                  onChange={HandleGroupname}
+                  className="py-4 px-3 border border-[#000] rounded-lg bg-ThirdColor text-[#fff] text-lg font-medium font-Nunito shadow-md shadow-Primary outline-none "
+                  type="text"
+                  placeholder="Enter your Group name"
                 />
-                <div className="">
-                  <h3 className="text-lg font-semibold font-Nunito text-ThirdColor">
-                  Friends Reunion
-                  </h3>
-                  <p className="text-sm font-medium text-FourColor">
-                  Hi Guys, Wassup!
-                  </p>
-                </div>
+                {Loader ? (
+                 <div className="text-lg font-medium font-Nunito text-[#fff] bg-ThirdColor  px-3 rounded-lg mt-3 shadow-md shadow-Secondary flex justify-center items-center">
+                   <InfinitySpin
+                    visible={true}
+                    width="120"
+                    color="#fff"
+                    ariaLabel="infinity-spin-loading"
+                  />
+                 </div>
+                ) : (
+                  <button
+                    onClick={HandleSubmitGroup}
+                    className="text-lg font-medium font-Nunito text-[#fff] bg-Secondary py-2 px-3 rounded-lg mt-3 shadow-md shadow-Secondary"
+                  >
+                    {Loader && <PacmanLoader />}
+                    Create Group
+                  </button>
+                )}
               </div>
-              <button className="px-4 py-2 bg-Secondary font-semibold font-Nunito text-[#fff] rounded-[5px] ">
-             Join
-                </button>
-            </div>
-            <div className="flex justify-between items-center  border-b border-black/25 pb-3 mt-4">
-              <div className="flex gap-3 mt-[17px]">
-                <img
-                  src={profileimage3}
-                  alt="progileimage"
-                  className="w-[52px] h-[52px] rounded-full object-cover"
-                />
-                <div className="">
-                  <h3 className="text-lg font-semibold font-Nunito text-ThirdColor">
-                  Friends Forever
-                  </h3>
-                  <p className="text-sm font-medium text-FourColor font-Nunito">Good to see you.</p>
+            ) : (
+           
+            <>
+            {Groupdata.map((item)=>(
+                <div className="flex justify-between items-center  border-b border-black/25 pb-3 mt-4">
+                <div className="flex gap-3 mt-[17px]">
+                  <img
+                    src={Profileimage2}
+                    alt="progileimage"
+                    className="w-[52px] h-[52px] rounded-full object-cover"
+                  />
+                  <div className="">
+                    <h3 className="text-lg font-semibold font-Nunito text-ThirdColor">
+                     {item.groupName
+                     }
+                    </h3>
+                    <p className="text-sm font-medium text-FourColor">
+                    Created by : {item.AdminName}
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <button className="px-4 py-2 bg-Secondary font-semibold font-Nunito text-[#fff] rounded-[5px] ">
-             Join
+                <button className="px-4 py-2 bg-Secondary font-semibold font-Nunito text-[#fff] rounded-[5px] ">
+                  Join
                 </button>
-            </div>
-            <div className="flex justify-between items-center  border-b border-black/25 pb-3 mt-4">
-              <div className="flex gap-3 mt-[17px]">
-                <img
-                  src={progileimage}
-                  alt="progileimage"
-                  className="w-[52px] h-[52px] rounded-full object-cover"
-                />
-                <div className="">
-                  <h3 className="text-lg font-semibold font-Nunito text-ThirdColor">
-                  Hi Guys
-                  </h3>
-                  <p className="text-sm font-medium text-FourColor font-Nunito">
-                  What plans today?
-                  </p>
-                </div>
               </div>
-              <button className="px-4 py-2 bg-Secondary font-semibold font-Nunito text-[#fff] rounded-[5px] ">
-             Join
-                </button>
-            </div>
-            <div className="flex justify-between items-center  border-b border-black/25 pb-3 mt-4">
-              <div className="flex gap-3 mt-[17px]">
-                <img
-                  src={profileimage3}
-                  alt="progileimage"
-                  className="w-[52px] h-[52px] rounded-full object-cover"
-                />
-                <div className="">
-                  <h3 className="text-lg font-semibold font-Nunito text-ThirdColor">
-                  Crazy Cousins
-                  </h3>
-                  <p className="text-xs font-normal text-FourColor/75">
-                  Hi Guys, Wassup!
-                  </p>
-                </div>
-              </div>
-              <button className="px-4 py-2 bg-Secondary font-semibold font-Nunito text-[#fff] rounded-[5px] ">
-             Join
-                </button>
-            </div>
-            <div className="flex justify-between items-center  border-b border-black/25 pb-3 mt-4">
-              <div className="flex gap-3 mt-[17px]">
-                <img
-                  src={profileimage3}
-                  alt="progileimage"
-                  className="w-[52px] h-[52px] rounded-full object-cover"
-                />
-                <div className="">
-                  <h3 className="text-lg font-semibold font-Nunito text-ThirdColor">
-                    Kiran
-                  </h3>
-                  <p className="text-xs font-normal text-FourColor/75">
-                  Good to see you.
-                  </p>
-                </div>
-              </div>
-              <button className="px-4 py-2 bg-Secondary font-semibold font-Nunito text-[#fff] rounded-[5px] ">
-             Join
-                </button>
-            </div>
+            ))}
+            </>
+            )}
           </div>
         </div>
       </div>
--- src/Components/MyGroup.jsx
+++ src/Components/MyGroup.jsx
@@ -1,121 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CiSearch } from "react-icons/ci";
 import { BsThreeDotsVertical } from "react-icons/bs";
-import progileimage from "../assets/mahtab.jpg";
-import Profileimage2 from "../assets/Signin.png";
-import profileimage3 from "../assets/Signup.jpg";
+import ProfileImage from "../assets/Signin.png"; 
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useSelector } from "react-redux";
 
 const MyGroup = () => {
+  const [groupData, setGroupData] = useState([]);
+  const db = getDatabase();
+  const data = useSelector((state) => state.UserData.value);
+
+  useEffect(() => {
+    const groupRef = ref(db, "grouplist/");
+    onValue(groupRef, (snapshot) => {
+      const groupArray = [];
+      snapshot.forEach((item) => {
+        if (data.displayName === item.val().AdminName) {
+          groupArray.push(item.val());
+        }
+      });
+      setGroupData(groupArray);
+    });
+  }, [db, data.displayName]);
+
   return (
     <section>
-      <div className="  ">
-        <div className="w-[427px] shadow-xl rounded-[20px] mt-[43px]  py-4 px-6 ">
+      <div>
+        <div className="w-[427px] shadow-xl rounded-[20px] mt-[43px] py-4 px-6">
           <div className="flex justify-between items-center">
             <h3 className="text-lg text-ThirdColor font-semibold font-Nunito">
-            My Groups
+              My Groups
             </h3>
             <BsThreeDotsVertical className="text-Secondary" />
           </div>
 
-          <div className="w-full h-[300px] overflow-y-scroll cursor-pointer ">
-            <div className="flex justify-between items-center  border-b border-black/25 pb-6 mt-4">
-              <div className="flex gap-3 mt-[17px]">
-                <img
-                  src={Profileimage2}
-                  alt="progileimage"
-                  className="w-[52px] h-[52px] rounded-full object-cover"
-                />
-                <div className="">
-                  <h3 className="text-sm font-semibold font-Nunito text-ThirdColor">
-                    Raghav
-                  </h3>
-                  <p className="text-xs font-normal text-FourColor/75">
-                    Dinner?
-                  </p>
+          <div className="w-full h-[300px] overflow-y-scroll cursor-pointer">
+            {groupData.map((item) => (
+              <div key={item.id} className="flex justify-between items-center border-b border-black/25 pb-6 mt-4">
+                <div className="flex gap-3 mt-[17px]">
+                  <img
+                    src={ProfileImage}
+                    alt="Profile"
+                    className="w-[52px] h-[52px] rounded-full object-cover"
+                  />
+                  <div>
+                    <h3 className="text-lg font-semibold font-Nunito text-ThirdColor">
+                      {item.groupName}
+                    </h3>
+                    <p className=" text-base font-medium text-FourColor font-Nunito">
+                    Created by : <span className="text-xs font-normal text-FourColor/75">{item.AdminName}</span>
+                    </p>
+                  </div>
                 </div>
+                <p className="text-[10px] font-normal font-Nunito text-ThirdColor/50">
+                  Today, 8:56pm
+                </p>
               </div>
-              <p className="text-[10px] font-normal font-Nunito text-ThirdColor/50">
-                Today, 8:56pm
-              </p>
-            </div>
-            <div className="flex justify-between items-center  border-b border-black/25 pb-6 mt-4">
-              <div className="flex gap-3 mt-[17px]">
-                <img
-                  src={profileimage3}
-                  alt="progileimage"
-                  className="w-[52px] h-[52px] rounded-full object-cover"
-                />
-                <div className="">
-                  <h3 className="text-sm font-semibold font-Nunito text-ThirdColor">
-                    Swathi
-                  </h3>
-                  <p className="text-xs font-normal text-FourColor/75">Sure!</p>
-                </div>
-              </div>
-              <p className="text-[10px] font-normal font-Nunito text-ThirdColor/50">
-                Today, 2:31pm
-              </p>
-            </div>
-            <div className="flex justify-between items-center  border-b border-black/25 pb-6 mt-4">
-              <div className="flex gap-3 mt-[17px]">
-                <img
-                  src={progileimage}
-                  alt="progileimage"
-                  className="w-[52px] h-[52px] rounded-full object-cover"
-                />
-                <div className="">
-                  <h3 className="text-sm font-semibold font-Nunito text-ThirdColor">
-                    Kiran
-                  </h3>
-                  <p className="text-xs font-normal text-FourColor/75">
-                    Hi.....
-                  </p>
-                </div>
-              </div>
-              <p className="text-[10px] font-normal font-Nunito text-ThirdColor/50">
-                Yesterday, 6:22pm
-              </p>
-            </div>
-            <div className="flex justify-between items-center  border-b border-black/25 pb-6 mt-4">
-              <div className="flex gap-3 mt-[17px]">
-                <img
-                  src={profileimage3}
-                  alt="progileimage"
-                  className="w-[52px] h-[52px] rounded-full object-cover"
-                />
-                <div className="">
-                  <h3 className="text-sm font-semibold font-Nunito text-ThirdColor">
-                    Kiran
-                  </h3>
-                  <p className="text-xs font-normal text-FourColor/75">
-                    Hi.....
-                  </p>
-                </div>
-              </div>
-              <p className="text-[10px] font-normal font-Nunito text-ThirdColor/50">
-                Today, 12:22pm
-              </p>
-            </div>
-            <div className="flex justify-between items-center  border-b border-black/25 pb-6 mt-4">
-              <div className="flex gap-3 mt-[17px]">
-                <img
-                  src={profileimage3}
-                  alt="progileimage"
-                  className="w-[52px] h-[52px] rounded-full object-cover"
-                />
-                <div className="">
-                  <h3 className="text-sm font-semibold font-Nunito text-ThirdColor">
-                    Kiran
-                  </h3>
-                  <p className="text-xs font-normal text-FourColor/75">
-                    Hi.....
-                  </p>
-                </div>
-              </div>
-              <p className="text-[10px] font-normal font-Nunito text-ThirdColor/50">
-                Today, 12:22pm
-              </p>
-            </div>
+            ))}
           </div>
         </div>
       </div>
